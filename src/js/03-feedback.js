47a11e4857ff3315a81bb7791208e257cb0677ad
Stiroid <stakhovsky89@gmail.com>
2023-03-07 15:07:17 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
-1
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+const STORAGE_KEY = 'feedback-form-state';
+let savedItems = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+  email: '',
+  message: '',
+};
+recordTextInput();
+function onFormInput(event) {
+  savedItems[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedItems));
+}
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (!event.target.email.value || !event.target.message.value)  {
+    alert ('cell must be filled!')
+    return;
+  } 
+const formState = localStorage.getItem(STORAGE_KEY);
+console.log(JSON.parse(formState));
+event.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+savedItems = { email: '', message: '' };
+}
+function recordTextInput() {
+   
+        form.email.value = savedItems.email;
+        form.message.value = savedItems.message;
+      
+}
